7b5c7d78c1574fbc0d43f9785d5cac54f01ce390
Daniel Almeida <almeida.danny@gmail.com>
2025-02-21 11:28:56 -0600

    add json translation to server.js file

--- server.js
+++ server.js
@@ -8,30 +8,46 @@ client.connect();
 const express = require(`express`);
 const app = express();
 
-app.get(`/api/v1/customers`, async (req, res) => {
+app.use(express.json());
 
+app.get(`/api/v1/customers`, async (req, res, next) => {
+  try{
   const getAllCustomers = await fetchAllCustomers();
   res.send(getAllCustomers);
+  } catch(err){
+    next(err);
+  }
 })
 
-app.get(`/api/v1/restaurants`, async (req, res) => {
-
+app.get(`/api/v1/restaurants`, async (req, res, next) => {
+try{
   const getAllRestaurants = await fetchAllRestaurants();
   res.send(getAllRestaurants);
+} catch(err){
+  next(err);
+}
 })
 
-app.get(`/api/v1/reservations`, async (req, res) => {
-
+app.get(`/api/v1/reservations`, async (req, res, next) => {
+try{
   const getAllReservations = await fetchAllReservations();
   res.send(getAllReservations);
+} catch (err){
+  next(err);
+}
 })
 
-app.post(`/api/v1/customers/:id/reservations`, async (req, res) => {
+app.post(`/api/v1/customers/:id/reservations`, async (req, res, next) => {
+  
   const { id } = req.params;
   const { restaurant_id, date, party_count } = req.body;
-
+  try{
+  
   const newReservation = await createReservations(date, party_count, restaurant_id, id)
   res.status(201).send(newReservation);
+  } catch (err){
+    next(err);
+  }
 })
 
 app.delete(`/api/v1/:customerid/reservations/:id`, async (req, res) => {
